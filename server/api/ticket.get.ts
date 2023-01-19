c7c2a1a8474b1a827bf5f6e572c70e02510855cd
lnkkerst <lnkkerst04@gmail.com>
2023-01-19 14:52:29 +0800

    feat: /api/ticket cors

--- server/api/ticket.get.ts
+++ server/api/ticket.get.ts
@@ -1,4 +1,17 @@
-export default defineEventHandler(async () => {
+export default defineEventHandler(async event => {
+  setResponseHeaders(event, {
+    'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Expose-Headers': '*'
+  });
+  if (getMethod(event) === 'OPTIONS') {
+    event.node.res.statusCode = 204;
+    event.node.res.statusMessage = 'No Content.';
+    return 'OK';
+  }
+
   const unusedTickets: any[] =
     (await useStorage().getItem('fs:tickets/unused')) ?? [];
   if (unusedTickets.length === 0) {
